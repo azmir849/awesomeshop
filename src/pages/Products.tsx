ae818eb09f6572c61de5340927122adfc994c796
azmir849 <azmir15-849@diu.edu.bd>
2021-11-20 11:51:37 +0600

    Navbar product page created

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+//Import products from index
+import Index from './Index'
+
 interface Props {
 
 }
 
 const Products: React.FC<Props> = () => {
-        return <div>Products</div>
+        return <Index/>
 }
 
 export default Products
