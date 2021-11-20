78e6f26bcbc64f362de7256374b1e66bad843ced
azmir849 <azmir15-849@diu.edu.bd>
2021-11-20 12:06:48 +0600

    Custome hook created in productdetails page

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+//import custom Hook
+import {useParams} from 'react-router-dom'
+
 interface Props {
 
 }
 
 const ProductDetail: React.FC<Props> = () => {
+        const params = useParams();
+        console.log(params)
         return <div>ProductDetail</div>
 }
 
