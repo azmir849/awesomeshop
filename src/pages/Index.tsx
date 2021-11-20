fe2ab354190da377ae5c0d3857fa90e11dcd1291
azmir849 <azmir15-849@diu.edu.bd>
2021-11-20 11:37:37 +0600

    product page created with fixed css

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
+//Import products from data
+import {products} from '../data/products'
+
+//Import ProductItem from products components
+import ProductItem from '../components/products/ProductItem'
+
 interface Props {
 
 }
 
 const index: React.FC<Props> = () => {
-        return <div>index</div>
+        return <div className="page--products">
+                <div className="products">
+                        {products.map(product =><ProductItem key={product.id} product={product} />)}
+                </div>
+
+        </div>
 }
 
 export default index
