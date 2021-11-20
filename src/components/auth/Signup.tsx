b7880750a62cf8f79ae68245f274d1919c2d08cb
azmir849 <azmir15-849@diu.edu.bd>
2021-11-20 16:34:11 +0600

    Signup page created

---
+++ src/components/auth/Signup.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const Signup: React.FC<Props> = () => {
+        return <>
+        <div className="backdrop"></div>
+
+        <div className="modal modal--auth-form">
+            <div className="modal-close">&times;</div>
+
+
+          <h3 className="header--center paragraph--orange">
+              Sign up to aswesomeshop
+          </h3>
+
+            <form className="form">
+                <div className="form__input-container">
+                    <label htmlFor="username" className="form__input-label">
+                        Username
+                    </label>
+                    <input type="text" name="username" className="input" placeholder="Your Username" />
+                </div>
+                <div className="form__input-container">
+                <label htmlFor="email" className="form__input-label">
+                        Email
+                    </label>
+                    <input type="email" name="email" className="input" placeholder="Your Email" />
+                </div>
+                <div className="form__input-container">
+                <label htmlFor="password" className="form__input-label">
+                        Password
+                    </label>
+                    <input type="password" name="password" className="input" placeholder="Your Password" />
+                </div>
+            </form>
+        </div>
+        </>
+}
+
+export default Signup
