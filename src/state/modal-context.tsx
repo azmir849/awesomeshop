6e2529ee750b585d92170452e771b6786f1dd279
azmir849 <azmir15-849@diu.edu.bd>
2021-11-21 15:54:54 +0600

    Create modal-context in state foler for handling signup/signin modal

---
+++ src/state/modal-context.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const ModalContextProvider: React.FC<Props> = () => {
+        return <div>modal-context</div>
+}
+
+export default ModalContextProvider
