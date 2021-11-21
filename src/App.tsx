b209c3aa566cbb27ab51c43d28f5a53febc47414
azmir849 <azmir15-849@diu.edu.bd>
2021-11-21 17:42:59 +0600

    appllied modal context in app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,16 @@ import Routes from './routes/Routes'
 //Import fontawesome
 import './fontawesome/index'
 
+//Import modalContextProvider from state
+import ModalContextProvider from './state/modal-context';
+
 function App() {
   return (
-    <div className="App">
+      <ModalContextProvider>
      <BrowserRouter>
         <Routes />
      </BrowserRouter>
-    </div>
+     </ModalContextProvider>
   );
 }
 
