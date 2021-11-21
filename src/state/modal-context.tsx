f2c69356c3f59d91b0de197835d8cbf869924bf4
azmir849 <azmir15-849@diu.edu.bd>
2021-11-21 16:26:36 +0600

    modal context initialized

--- src/state/modal-context.tsx
+++ src/state/modal-context.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React,{createContext, Dispatch, ReactElement, SetStateAction, useEffect, useState} from 'react'
+import Signup from '../components/auth/Signup'
 
 interface Props {
 
 }
 
-const ModalContextProvider: React.FC<Props> = () => {
-        return <div>modal-context</div>
+type ModalType = 'close' | 'signup' | 'signin' | 'reset_password'
+
+type ModalState = {
+    modal: ReactElement | null
+    setModalType: Dispatch<SetStateAction<ModalType>>
+}
+
+type Modals = {
+    [key in ModalType]: ReactElement | null
+}
+
+const ModalContext =  createContext<ModalState | undefined>(undefined)
+
+const modals: Modals = {
+    close: null,
+    signup: <Signup />,
+    signin: null,
+    reset_password: null
+}
+
+const ModalContextProvider: React.FC<Props> = ({children}) => {
+    const[modal,setModal]=useState<ReactElement | null>(null)
+    const[modalType,setModalType] = useState<ModalType>('close')
+
+    useEffect(() => {
+        setModal(modals[modalType])
+    }, [modalType])
+
+        return <ModalContext.Provider value={{modal, setModalType}} >{children}</ModalContext.Provider>
 }
 
 export default ModalContextProvider
