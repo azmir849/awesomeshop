509a73b6395e7126fb520ac42af30654ff0d3c2a
azmir849 <azmir15-849@diu.edu.bd>
2021-11-22 14:36:56 +0600

    faUserCiorcle imported

--- src/fontawesome/index.ts
+++ src/fontawesome/index.ts
@@ -1,4 +1,4 @@
 import {library} from '@fortawesome/fontawesome-svg-core'
-import {faPlus,faMinus,faCartArrowDown} from '@fortawesome/free-solid-svg-icons'
+import {faPlus,faMinus,faCartArrowDown,faUserCircle} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faPlus,faMinus,faCartArrowDown)
+library.add(faPlus,faMinus,faCartArrowDown,faUserCircle)
