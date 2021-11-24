73bc8bc5beb1bb103e464ca5e209bd49b1200b4e
azmir849 <azmir15-849@diu.edu.bd>
2021-11-24 10:32:32 +0600

    Sidebar import in Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/Nav/Navbar'
+import Sidebar from './components/Nav/Sidebar'
 
 import {useModalContext} from './state/modal-context'
 
@@ -11,6 +12,7 @@ const Layout: React.FC<Props> = ({children}) => {
       const {modal} =  useModalContext()
         return <div>
           <Navbar/>
+          <Sidebar/>
 
           <div className="page">{children}</div>
             {modal && modal}
