23b63be8dd3382c149ba1ccf61f5eb86e63721b9
azmir849 <azmir15-849@diu.edu.bd>
2021-11-25 12:47:41 +0600

    Signup and resetPassword is linked up in bottom of signup page

--- src/components/auth/Signin.tsx
+++ src/components/auth/Signin.tsx
@@ -1,96 +1,113 @@
-import React from 'react'
+import React from "react";
 
 //Import custom button
-import Button from '../Button'
+import Button from "../Button";
 
 //Import custom Input with error hamndler
-import Input from '../Input'
+import Input from "../Input";
 
 //Import custom hook from state/modal-context
-import {useModalContext} from '../../state/modal-context'
+import { useModalContext } from "../../state/modal-context";
 
 //Import react hook form
-import {useForm} from 'react-hook-form'
-import { SignupData } from '../../types/index'
+import { useForm } from "react-hook-form";
+import { SignupData } from "../../types/index";
 
 //Import custom hooks
-import{useAuthenticate} from '../hooks/index'
+import { useAuthenticate } from "../hooks/index";
 
-interface Props {
-
-}
+interface Props { }
 
 const Signin: React.FC<Props> = () => {
-        const {setModalType} = useModalContext()
-        const {signin,loading,error} = useAuthenticate()
-        const{register,errors, handleSubmit} = useForm<Omit<SignupData, 'username'>>()
+    const { setModalType } = useModalContext();
+    const { signin, loading, error } = useAuthenticate();
+    const { register, errors, handleSubmit } =
+        useForm<Omit<SignupData, "username">>();
 
-        const handleSignin = handleSubmit( async (data)=>{
-            const response = await signin(data)
+    const handleSignin = handleSubmit(async (data) => {
+        const response = await signin(data);
 
-            if(response) setModalType('close')
+        if (response) setModalType("close");
 
-            // console.log(data)
-            // signup(data)
-            // window.alert('Submitted successfully')
-            // setModalType('close')
-        })
+        // console.log(data)
+        // signup(data)
+        // window.alert('Submitted successfully')
+        // setModalType('close')
+    });
 
-       
-        return <>
-        <div className="backdrop"  ></div>
+    return (
+        <>
+            <div className="backdrop"></div>
 
-        <div className="modal modal--auth-form">
-            <div className="modal-close" onClick={()=> setModalType('close') } >&times;</div>
+            <div className="modal modal--auth-form">
+                <div className="modal-close" onClick={() => setModalType("close")}>
+                    &times;
+                </div>
 
+                <h3 className="header--center paragraph--orange">
+                    Sign in to Aswesomeshop
+                </h3>
 
-          <h3 className="header--center paragraph--orange">
-              Sign in to Aswesomeshop
-          </h3>
+                <form className="form" onSubmit={handleSignin}>
+                    <Input
+                        label="Email"
+                        name="email"
+                        placeholder="Your Email"
+                        error={errors.email?.message}
+                        ref={register({
+                            required: "Email is required",
+                            // pattern: {
+                            //     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            //     message: 'Your email is in wrong format'
+                            // }
+                        })}
+                    />
+                    <Input
+                        label="Password"
+                        name="password"
+                        type="password"
+                        placeholder="Your Password"
+                        error={errors.password?.message}
+                        ref={register({
+                            required: "Password is required",
+                            minLength: {
+                                value: 6,
+                                message: "Password must be at least 6 characters",
+                            },
+                            maxLength: {
+                                value: 50,
+                                message: "Password must not be greater than 50 characters",
+                            },
+                        })}
+                    />
 
-            <form className="form" onSubmit={handleSignin}>    
-            <Input 
-                label='Email'
-                name="email"
-                placeholder='Your Email' 
-                error={errors.email?.message}
-                ref={register({  
-                    required: 'Email is required',
-                    // pattern: {
-                    //     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    //     message: 'Your email is in wrong format'
-                    // }
+                    <Button loading={loading} width="100%" style={{ margin: "0.5rem 0" }}>
+                        Submit
+                    </Button>
+                </form>
+                {error && <p className="paragraph paragraph--error">{error}</p>}
+                <p className="paragraph paragraph--focus paragraph--small">
+                    Don't have an account yet?{" "}
+                    <span
+                        className="paragraph--orange paragraph--link"
+                        onClick={() => setModalType("signup")}
+                    >
+                        Sign up
+                    </span>
+                </p>
 
-                    }
-                )}
-                />
-            <Input 
-                label='Password'
-                name="password"
-                type='password'
-                placeholder='Your Password' 
-                error={errors.password?.message}
-                ref={register({  
-                    required: 'Password is required',
-                    minLength: {
-                        value: 6,
-                        message: 'Password must be at least 6 characters'
-                    },
-                    maxLength: {
-                        value: 50,
-                        message: 'Password must not be greater than 50 characters'
-                    },
-
-                    }
-                )}
-                />
-
-                <Button loading={loading} width='100%' style={{margin: '0.5rem 0'}}>Submit</Button>
-            </form>
-
-            {error && <p className='paragraph paragraph--error'>{error}</p>}
-        </div>
+                <p className="paragraph paragraph--focus paragraph--small">
+                    Forgot your password? Click{" "}
+                    <span
+                        className="paragraph--orange paragraph--link"
+                        onClick={() => setModalType("reset_password")}
+                    >
+                        here
+                    </span>
+                </p>
+            </div>
         </>
-}
+    );
+};
 
-export default Signin
+export default Signin;
