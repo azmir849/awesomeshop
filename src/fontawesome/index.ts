88056a780a52719f303e8ff88478943dabc0ef8b
azmir849 <azmir15-849@diu.edu.bd>
2021-11-27 10:30:23 +0600

    fontawesome brand icon imported

--- src/fontawesome/index.ts
+++ src/fontawesome/index.ts
@@ -1,4 +1,6 @@
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {faPlus,faMinus,faCartArrowDown,faUserCircle} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faPlus,faMinus,faCartArrowDown,faUserCircle)
+import {faFacebookF, faGoogle} from '@fortawesome/free-brands-svg-icons'
+ 
+library.add(faPlus,faMinus,faCartArrowDown,faUserCircle,faFacebookF,faGoogle)
